5e9561513fcd9769a48e9f9796a398f33e42723f
hamidfarid17 <hamid.farid@gmail.com>
2023-05-16 11:44:41 +0330

    improvement minmax

--- mainUI.py
+++ mainUI.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
     players.append(AI.QNeural())
     # players.append(AI.QTable())
     # players.append(AI.QTable())
-    players.append(Algorithm.Random())
+    # players.append(Algorithm.Random())
     # players.append(Algorithm.Human())
     # players.append(Algorithm.MinMax(1))
     # players.append(Algorithm.MinMax(2))
     # players.append(Algorithm.MinMax(3))
     # players.append(Algorithm.MinMax(4))
-    # players.append(Algorithm.MinMax(5))
+    players.append(Algorithm.MinMax(5))
 
     t = time.time()
-    play(players, 50, 10000, sweep=True, rst=True, league=True)
+    play(players, 50, 100, sweep=True, rst=True, league=True)
     print(time.time() - t)
 
 
